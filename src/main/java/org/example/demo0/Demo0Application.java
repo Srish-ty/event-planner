71617024e5fa30affbbf3583065248a611b0b357
Srishty <srishtymangutte@gmail.com>
2025-06-11 13:11:13 +0530

    feat: added root endpoint

--- src/main/java/org/example/demo0/Demo0Application.java
+++ src/main/java/org/example/demo0/Demo0Application.java
@@ -2,12 +2,20 @@ package org.example.demo0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo0Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo0Application.class, args);
+    }
+    @GetMapping("/")
+    public String index() {
+        return "The / page";
     }
 
 }
